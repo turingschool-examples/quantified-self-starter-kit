25b11b9fb8fbb1b8bc9e2c94e78e738a1f33f029
Chris Smith <42@chrisryansmith.com>
2017-08-18 10:17:09 -0600

    MealTotal class can calculate cals consumed and remaining

--- test/models/MealTotals-test.js
+++ test/models/MealTotals-test.js
@@ -1,4 +1,6 @@
 const assert = require('chai').assert;
+const Meal = require('../../lib/models/Meal.js')
+const Food = require('../../lib/models/Food.js');
 const MealTotals = require('../../lib/models/MealTotals.js');
 
 describe('meal', () => {
@@ -19,6 +21,44 @@ describe('meal', () => {
     });
   });
 
-  describe('.caloriesConsumed', () => {})
-  describe('.remainingCalories', () => {})
+  describe('.caloriesConsumed', () => {
+    context('when there are no calories in meals', () => {
+      it('it returns 0', () => {
+        const myMealTotals = new MealTotals();
+
+        assert.equal(myMealTotals.caloriesConsumed(), 0);
+      })
+    })
+    context('when a meal has calories', () => {
+      it('it returns the sum', () => {
+        const myBFood = new Food( {name: "Test", calories: 100} );
+        const myBMeal = new Meal( {name: "Breakfast", foods: [myBFood]} );
+        const myLFood = new Food( {name: "Test", calories: 200} );
+        const myLMeal = new Meal( {name: "Lunch", foods: [myLFood]} );
+        const myMealTotals = new MealTotals( {meals: [myBMeal, myLMeal]} );
+
+        assert.equal(myMealTotals.caloriesConsumed(), 300);
+      })
+    })
+  })
+  describe('.remainingCalories', () => {
+    context('when there are no calories in meals', () => {
+      it('it returns 2000', () => {
+        const myMealTotals = new MealTotals();
+
+        assert.equal(myMealTotals.remainingCalories(), 2000);
+      })
+    })
+    context('when a meal has calories', () => {
+      it('it returns the sum', () => {
+        const myBFood = new Food( {name: "Test", calories: 100} );
+        const myBMeal = new Meal( {name: "Breakfast", foods: [myBFood]} );
+        const myLFood = new Food( {name: "Test", calories: 200} );
+        const myLMeal = new Meal( {name: "Lunch", foods: [myLFood]} );
+        const myMealTotals = new MealTotals( {meals: [myBMeal, myLMeal]} );
+
+        assert.equal(myMealTotals.remainingCalories(), 1700);
+      })
+    })
+  })
 });
