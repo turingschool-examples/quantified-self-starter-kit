94533b45bc00338370ef1a7672daab1f4c99bfd1
rwilhel <rwilhel@gmail.com>
2017-08-18 23:31:30 -0600

    Builds out test for loading foods onto index.html

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,35 @@
 const assert = require('chai').assert
+const webdriver = require('selenium-webdriver')
+const until = webdriver.until
+const test = require('selenium-webdriver/testing')
+const frontEndLocation = "http://localhost:8080"
 
 describe('our test bundle', function() {
 	it('works', function() {
 		assert(true)
+	})
+})
+
+test.describe('testing index page', function() {
+	let driver
+	this.timeout(10000)
+
+	test.beforeEach(function () {
+		driver = new webdriver.Builder()
+		.forBrowser('chrome')
+		.build()
+	})
+
+	test.afterEach(function () {
+		driver.quit()
+	})
+
+	test.it("lists all foods on load", function() {
+		driver.get(`${frontEndLocation}`)
+		driver.wait(until.elementLocated({css: "#foods .food-item"}))
+		driver.findElements({css: "#foods .food-item"})
+		.then(function (foods) {
+			assert.lengthOf(foods, 12)
+		})
 	})
 })
