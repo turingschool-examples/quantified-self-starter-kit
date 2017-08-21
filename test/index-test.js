1a85e3677b98f47159d96c9e0f7060aaf862fac1
rwilhel <rwilhel@gmail.com>
2017-08-20 21:06:03 -0600

    Passes test for food data populating on index.html page

--- test/index-test.js
+++ test/index-test.js
@@ -1,36 +1,37 @@
-// const assert = require('chai').assert
-// const webdriver = require('selenium-webdriver')
-// const until = webdriver.until
-// const test = require('selenium-webdriver/testing')
-// const frontEndLocation = "http://localhost:8080"
-// const fs = require('fs')
-// 
-// describe('our test bundle', function() {
-// 	it('works', function() {
-// 		assert(true)
-// 	})
-// })
-//
-// test.describe('testing index page', function() {
-// 	let driver
-// 	this.timeout(10000)
-//
-// 	test.beforeEach(function () {
-// 		driver = new webdriver.Builder()
-// 		.forBrowser('chrome')
-// 		.build()
-// 	})
-//
-// 	test.afterEach(function () {
-// 		driver.quit()
-// 	})
-//
-// 	test.it("lists all foods on load", function() {
-// 		driver.get(`${frontEndLocation}`)
-// 		driver.wait(until.elementLocated({css: "#foods .food-item"}))
-// 		driver.findElements({css: "#foods .food-item"})
-// 		.then(function (foods) {
-// 			assert.lengthOf(foods, 12)
-// 		})
-// 	})
-// })
+const assert = require('chai').assert
+const webdriver = require('selenium-webdriver')
+const until = webdriver.until
+const test = require('selenium-webdriver/testing')
+const frontEndLocation = "http://localhost:8080"
+
+test.describe('Our test bundle', function() {
+	it('works', function() {
+		assert(true)
+	})
+})
+
+test.describe('Testing Home/Index Page', function() {
+	let driver
+	this.timeout(10000)
+
+	test.beforeEach(function () {
+		driver = new webdriver.Builder()
+		.forBrowser('chrome')
+		.build()
+	})
+
+	test.afterEach(function () {
+		driver.quit()
+	})
+
+	test.it("lists all foods from database on load", function() {
+		driver.get(`${frontEndLocation}`)
+		driver.wait(until.elementLocated({css: '.food-name'}))
+		driver.findElements({css: '.food-name'})
+		.then(function(foods) {
+      foods.forEach(function(food) {
+        assert(food.isDisplayed())
+      })
+		})
+	})
+})
