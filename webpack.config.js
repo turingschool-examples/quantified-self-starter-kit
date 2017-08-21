74a9be51064fadf61a72b448343023b099f33c9b
rwilhel <rwilhel@gmail.com>
2017-08-21 16:41:43 -0600

    Adds test files

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: {
     main: "./lib/index.js",
-    test: "mocha!./test/index-test.js"
+    test: "mocha!./test/index.js"
   },
   node: {
     fs: 'empty'
