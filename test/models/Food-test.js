2a77c255db8d849e886e4610b4a4b81d5ff08045
rongxanh88 <rongxanh88@gmail.com>
2017-08-23 09:11:12 -0600

    beginning refactor by styling according to AirBnB standards

--- test/models/Food-test.js
+++ test/models/Food-test.js
@@ -5,26 +5,26 @@ const HtmlHelper = require('../../lib/helpers/HtmlHelper');
 describe('Food', () => {
   context('attributes', () => {
     it('it is a food', () => {
-      const myFood = new Food({name: "Test", calories: 100})
-      assert.instanceOf(myFood, Food)
-    })
+      const myFood = new Food({name: "Test", calories: 100});
+      assert.instanceOf(myFood, Food);
+    });
 
     it('it has a name', () => {
-      const myFood = new Food({name: "Test", calories: 100})
-      assert.equal(myFood.name, "Test")
-    })
+      const myFood = new Food({name: "Test", calories: 100});
+      assert.equal(myFood.name, "Test");
+    });
 
     it('it has calories', () => {
-      const myFood = new Food({name: "Test", calories: 100})
-      assert.equal(myFood.calories, 100)
-    })
-  })
+      const myFood = new Food({name: "Test", calories: 100});
+      assert.equal(myFood.calories, 100);
+    });
+  });
 
   context('methods', () => {
     it('turns food object to HTML', () => {
-      const deleteIcon = '../../vendor/assets/delete.png'
-      const attrs = { id: 1, name: 'Tomato', calories: 200 }
-      const food = new Food(attrs)
+      const deleteIcon = '../../vendor/assets/delete.png';
+      const attrs = { id: 1, name: 'Tomato', calories: 200 };
+      const food = new Food(attrs);
       const expectedHTML = `<tr class="food-row">` +
         `<td class="food-name" data-id="${attrs.id}">${attrs.name}</td>` +
         `<td class="food-calories number" data-id="${attrs.id}">${attrs.calories}</td>` +
