73147a0affae1fdf8a0fafa6c8b5a53e6471a8d5
djtrujillo <danny.j.trujillo@gmail.com>
2017-11-30 15:43:48 -0700

    Added foods to page and are able to add foods

--- main.bundle.js
+++ main.bundle.js
@@ -42,9 +42,78 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	
+	"use strict";
 
-/***/ }
+	var _events = __webpack_require__(1);
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _foods = __webpack_require__(2);
+
+	// if (window.location.pathname == '/foods.html') {
+
+	var onLoad = $(document).ready(function () {
+	  (0, _foods.getFoods)();
+	}); // const getFoods = require('../requests/foods')
+
+
+	var clickSubmit = $('#new-food-submit').on('click', function (event) {
+	  console.log('clicked');
+	  event.preventDefault();
+	  var food = $('#food').val();
+	  var calories = $('#calories').val();
+
+	  if (food === "") {
+	    alert("Please enter food");
+	  } else if (calories === "") {
+	    alert("Please enter calories");
+	  } else {
+	    (0, _foods.postFood)(food, calories);
+	  }
+	});
+	// }
+
+	module.exports = { onLoad: onLoad, clickSubmit: clickSubmit };
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+	"use strict";
+
+	var requestUrl = "http://localhost:3000/api/v1";
+
+	var tableRow = function tableRow(food) {
+	  return "<tr><td>" + food["name"] + "</td><td>" + food["calories"] + "</td></tr>";
+	};
+
+	var getFoods = function getFoods() {
+	  $.get(requestUrl + "/foods").then(function (foods) {
+	    foods.forEach(function (food) {
+	      $("#food-table").prepend(tableRow(food));
+	    });
+	  });
+	};
+
+	var postFood = function postFood(food, calories) {
+	  $.post(requestUrl + "/foods", {
+	    "food": {
+	      "name": food,
+	      "calories": calories
+	    }
+	  }).then(function () {
+	    location.reload(true);
+	    // getFoods()
+	  });
+	};
+
+	module.exports = { getFoods: getFoods, postFood: postFood };
+
+/***/ })
 /******/ ]);
