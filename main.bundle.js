0c11bbadcfceb44f42555fecbeea6c845d623668
Liam Barstad <liambarstad@gmail.com>
2018-01-27 13:21:46 -0700

    add editing button working, needs class

--- main.bundle.js
+++ main.bundle.js
@@ -10448,19 +10448,95 @@ Object.defineProperty(exports, "__esModule", {
 });
 var $ = __webpack_require__(0);
 var Food = __webpack_require__(5);
-var API = "https://turing-quantified-self-api.herokuapp.com/api/v1/";
+var API = 'https://ml-quantified-self.herokuapp.com/api/v1/';
 
 exports.getAllFoods = getAllFoods;
+exports.deleteFood = deleteFood;
+exports.postFood = postFood;
+exports.editBox = editBox;
+exports.editFood = editFood;
 
+
+function addInfo(id, name, calories, targetNode) {
+  targetNode.append('<td class="food-name" data-id="' + id + '">' + name + '</td>');
+  targetNode.append('<td class="food-calories" data-id="' + id + '">' + calories + '</td>');
+  targetNode.append('<td><img class="delete-food" data-id="' + id + '" src="./lib/assets/delete1.png"></td>');
+}
+
+function addFood(id, name, calories) {
+  var bottom = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
+
+  var newRow = '<tr class="food-' + id + '"></tr>';
+  if (bottom) {
+    $('#foods-table').append(newRow);
+  } else {
+    $(newElement).insertAfter('#foods-table-headers');
+  }
+  addInfo(id, name, calories, $('.food-' + id));
+}
 
 function getAllFoods() {
-  fetch('https://turing-quantified-self-api.herokuapp.com/api/v1/foods').then(function (response) {
+  fetch(API + 'foods').then(function (response) {
     return response.json();
   }).then(function (data) {
     data.forEach(function (food) {
-      $('table').append('<tr><td>' + food.name + '</td><td>' + food.calories + '</td><td><img src=\'../assets/delete1.png\'></td></tr>');
-      // $('#foods').append(`<td>${food.calories}</td>`);
+      addFood(food.id, food.name, food.calories);
     });
+  });
+}
+
+function deleteFood(target) {
+  var id = target.dataset.id;
+  fetch(API + 'foods/' + id.toString(), { method: 'DELETE' }).then(function (data) {
+    $('.food-' + id).remove();
+  });
+}
+
+function postFood() {
+  var name = $('#new-food-name input').val();
+  var calories = $('#new-food-calories input').val();
+  $('#new-food-form .alert').empty();
+  $('#new-food-name input').val('');
+  $('#new-food-calories input').val('');
+  if (name === "") {
+    $('#new-food-name').append('<p class="alert">Please enter a food name</p>');
+  } else if (calories === "") {
+    $('#new-food-calories').append('<p class="alert">Please enter a calorie amount</p>');
+  } else {
+    sendPostRequest(name, calories.toString());
+  }
+}
+
+function sendPostRequest(name, calories) {
+  fetch(API + 'foods', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ food: { name: name, calories: calories } })
+  }).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    return addFood(data.id, data.name, data.calories, false);
+  });
+}
+
+function editBox(target) {
+  var input = document.createElement('input');
+  input.type = "text";
+  input.className = "editBox";
+  input.dataset.id = target.dataset.id;
+  target.replaceWith(input);
+}
+
+function editFood(id, attr) {
+  var $inputBox = $('input[data-id=' + id + ']');
+  var body = { food: {} };
+  body['food'][attr] = $inputBox.val();
+  fetch(API + 'foods/' + id.toString(), {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  }).then(function (response) {
+    return console.log(response);
   });
 }
 
@@ -10478,8 +10554,41 @@ var $ = __webpack_require__(0);
 
 
 $(document).ready(function () {
+  var editing = false;
   (0, _foodService.getAllFoods)();
+  $('#foods-table').on('click', function (event) {
+    event.stopPropagation();
+    var target = event.target;
+    if (target.className == 'delete-food') {
+      (0, _foodService.deleteFood)(target);
+    } else if ((target.className == 'food-name' || 'food-calories') && !editing) {
+      (0, _foodService.editBox)(target);
+      editing = true;
+      addEditListener(target);
+    }
+  });
+  $('form input[value="submit"]').on('click', function (event) {
+    event.preventDefault();
+    (0, _foodService.postFood)();
+  });
 });
+
+function addEditButton(target) {
+  var id = target.dataset.id;
+  var deleteButton = $('td').find('img[data-id="' + id + '"]');
+  $('<button data-id="' + id + '">Edit</button>').insertAfter(deleteButton);
+}
+
+function addEditListener(target) {
+  addEditButton(target);
+  var id = target.dataset.id;
+  var attr = target.className.split("-")[1];
+  var $button = $('button[data-id=' + id + ']');
+  $($button).on('click', function (event) {
+    event.stopPropagation;
+    (0, _foodService.editFood)(id, attr);
+  });
+}
 
 /***/ },
 
